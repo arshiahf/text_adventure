7584a190dd8ed5531206655b4bba23f9d199508b
MTaurone <arshiahf@gmail.com>
2018-01-06 13:37:06 -0800

    Changed to use a str reference rather than String

--- src/environment/mod.rs
+++ src/environment/mod.rs
@@ -21,7 +21,7 @@ pub fn create_rooms(root_dir:String)
         println!("There is now a folder entitled {}, please fill in the returns for all locally accepted commands.", room_name);
         let builder = DirBuilder::new();
         let path:String = root_dir.clone() + &room_name;
-        builder.create(path).unwrap();
+        builder.create(&path).unwrap();
 
         let mut continuevec:Vec<char> = Vec::new();
         request_input_to_vec("Would you like to make more rooms y/n? ", &mut continuevec);
