05b5e32c6550d0a7da8b61653d7528e97d0a29f0
mtaurone <arshiahf@gmail.com>
2018-01-10 00:53:01 -0800

    Attempted use of Vector for str input. No avail.

--- src/environment/mod.rs
+++ src/environment/mod.rs
@@ -9,20 +9,19 @@ struct Room
     accepted_commands: Vec<String>,
 }
 
-pub fn create_rooms(root_dir:String)
+pub fn create_rooms(root_dir:Vec<str>)
 {
     let mut more_rooms: bool = true;
     while more_rooms
     {
-        let mut room_name: String = String::new();
-        request_input_string("Please type in the name of a room: ", &mut room_name);
+        let mut room_name:Vec<str> = Vec::new();
+        request_input_string("Please type in the name of a room with no spaces: ", &mut room_name);
         let mut accepted_commands: Vec<String> = Vec::new();
         request_input_to_vec("Please type all accepted commands for this room, separated by spaces: ", &mut accepted_commands);
         println!("There is now a folder entitled {}, please fill in the returns for all locally accepted commands.", room_name);
         let mut builder = DirBuilder::new();
-        let path = root_dir.clone() + &room_name;
-        let path_str:&str = &path[..];
-        builder.recursive(true).create(path_str).unwrap();
+        let path = root_dir.clone() + room_name[0];
+        builder.recursive(true).create(path).unwrap();
 
         let mut continuevec:Vec<char> = Vec::new();
         request_input_to_vec("Would you like to make more rooms y/n? ", &mut continuevec);
