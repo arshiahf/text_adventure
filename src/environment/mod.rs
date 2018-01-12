f5443aff0bcbbcb97804b648424171dd70536ec3
mtaurone <arshiahf@gmail.com>
2018-01-11 17:24:47 -0800

    Trying as_str() again

--- src/environment/mod.rs
+++ src/environment/mod.rs
@@ -9,23 +9,19 @@ struct Room
     accepted_commands: Vec<String>,
 }
 
-pub fn create_rooms(root_dir:Vec<str>)
+pub fn create_rooms(root_dir:&mut String)
 {
     let mut more_rooms: bool = true;
     while more_rooms
     {
-        let mut room_name:Vec<str> = Vec::new();
+        let mut room_name:String = String::new();
         request_input_string("Please type in the name of a room with no spaces: ", &mut room_name);
         let mut accepted_commands: Vec<String> = Vec::new();
         request_input_to_vec("Please type all accepted commands for this room, separated by spaces: ", &mut accepted_commands);
         println!("There is now a folder entitled {}, please fill in the returns for all locally accepted commands.", room_name);
         let mut builder = DirBuilder::new();
-        let path = root_dir.clone() + room_name[0];
-        let length = path.len();
-        let ptr = path.as_ptr();
-        let path_slice = slice::from_raw_parts(ptr, length);
-        str::from_utf8(path_slice);
-        builder.recursive(true).create(path_slice).unwrap();
+        let mut path = root_dir.clone() + &mut room_name;
+        builder.recursive(true).create(path.as_mut_str()).unwrap();
 
         let mut continuevec:Vec<char> = Vec::new();
         request_input_to_vec("Would you like to make more rooms y/n? ", &mut continuevec);
