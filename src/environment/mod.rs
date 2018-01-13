5fd8ac3d9330829cd9471a41975e03d808bd36fd
MTaurone <arshiahf@gmail.com>
2018-01-12 21:32:33 -0800

    Created room initialization function

--- src/environment/mod.rs
+++ src/environment/mod.rs
@@ -1,7 +1,12 @@
 // Generic module for environment variables and functions
 
-use user_input::requests::{request_input_string, request_input_to_vec};
-use std::fs::{File, DirBuilder};
+use user_input::requests::{
+    request_input_string,
+    request_input_to_vec,
+    new_directory,
+    new_files
+};
+use std::fs::{File};
 use std::process::{exit};
 
 struct Room
@@ -10,32 +15,15 @@ struct Room
     accepted_commands: Vec<String>,
 }
 
-pub fn create_rooms(root_dir:&mut String)
+pub fn create_rooms()
 {
+    let mut root_dir:String = String::new();
+    request_input_string("Please input root directory with no spaces: ", &mut root_dir);
+
     let mut more_rooms: bool = true;
     while more_rooms
     {
-        let mut room_name:String = String::new();
-        request_input_string("Please type in the name of a room: ", &mut room_name);
-        //room_name.pop();
-        //room_name.pop();
-        //room_name.push('\0');
-        let mut accepted_commands: Vec<String> = Vec::new();
-        request_input_to_vec("Please type all accepted commands for this room, separated by spaces: ", &mut accepted_commands);
-        println!("There is now a folder entitled {}, please fill in the returns for all locally accepted commands.", room_name);
-        let mut builder = DirBuilder::new();
-        //root_dir.pop();
-        //root_dir.pop();
-        //root_dir.push('\0');
-        let path = root_dir.clone() + &room_name;
-        match builder.recursive(true).create(path.as_str())
-        {
-            Ok(_) => {}
-            Err(err) => {
-                eprintln!("{} {:?}", path, err);
-                exit(1);
-            }
-        }
+        initialize_room(root_dir.clone());
 
         let mut continuevec:Vec<char> = Vec::new();
         request_input_to_vec("Would you like to make more rooms y/n? ", &mut continuevec);
@@ -47,7 +35,15 @@ pub fn create_rooms(root_dir:&mut String)
     }
 }
 
-fn initialize_room(name:String)
+fn initialize_room(root_dir:String)
 {
+    let mut room_name:String = String::new();
+    request_input_string("Please type in the name of a room: ", &mut room_name);
+    new_directory(root_dir.clone(), room_name.clone());
 
+    let mut accepted_commands:Vec<String> = Vec::new();
+    request_input_to_vec("Please type all accepted commands for this room, separated by spaces: ", &mut accepted_commands);
+    new_files(root_dir.clone(), room_name.clone(), accepted_commands);
+
+    println!("There is now a folder entitled {}, please fill in the returns for all locally accepted commands.", room_name);
 }
