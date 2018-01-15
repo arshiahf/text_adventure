7da7aea0510ed19a4727005dc6e8f01bb471fc2e
MTaurone <arshiahf@gmail.com>
2018-01-15 11:21:06 -0800

    Introducing use of Room struct. Yet to implement fully

--- src/environment/mod.rs
+++ src/environment/mod.rs
@@ -19,24 +19,27 @@ pub fn create_rooms()
 {
     let mut root_dir:String = String::new();
     request_input_string("Please input root directory with no spaces or slashes: ", &mut root_dir);
+    let mut commands:Vec<String> = Vec::new();
 
     let mut more_rooms: bool = true;
     while more_rooms
     {
-        initialize_room(root_dir.clone());
+        initialize_room(root_dir.clone(), commands);
 
         let mut continuevec:Vec<char> = Vec::new();
         request_input_to_vec("Would you like to make more rooms y/n? ", &mut continuevec);
         match continuevec[0]
         {
-            'y' => more_rooms = true,
+            'y'|"yes" => more_rooms = true,
             _ => more_rooms = false,
         }
     }
+    let commands_dir = root_dir.clone() + "/commands/";
+    new_files(commands_dir, commands);
     for
 }
 
-fn initialize_room(root_dir:String)
+fn initialize_room(root_dir:String, commands:Vec<String>) -> Room
 {
     let mut room_name:String = String::new();
     let rooms_dir = root_dir.clone() + "/rooms/";
@@ -44,10 +47,18 @@ fn initialize_room(root_dir:String)
     new_directory(rooms_dir.clone(), room_name.clone());
 
     let mut accepted_commands:Vec<String> = Vec::new();
-    request_input_to_vec("Please type all accepted commands for this room, separated by spaces: ", &mut accepted_commands);
-    let commands_dir = root_dir.clone() + "/commands/";
-    new_files(commands_dir, accepted_commands);
-    accepted_commands.push("description");
+    request_input_to_vec("Please type all accepted commands for this room, separated by spaces: ", &mut commands_list);
+    for command in commands_list
+    {
+        if !commands.contains(command)
+        {
+            commands.push(command);
+        }
+    }
+
 
     println!("There is now a folder entitled {}, please fill in the returns for all locally accepted commands.", room_name);
+
+    let mut new_room = struct Room{id:room_name, accepted_commands:commands_list};
+    new_room
 }
